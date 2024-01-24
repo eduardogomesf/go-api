55a4ffb0766d069b7c862ff39395499ec0b2b1bc
Eduardo Gomes <edugomesf@icloud.com>
2024-01-23 21:29:47 -0300

    test: add unit tests for product repository

--- internal/entity/product_test.go
+++ internal/entity/product_test.go
@@ -7,16 +7,16 @@ import (
 )
 
 func TestNewProduct(t *testing.T) {
-	p, err := NewProduct("Product 1", 10)
+	p, err := NewProduct("Product 1", 10.0)
 	assert.Nil(t, err)
 	assert.NotNil(t, p)
 	assert.NotEmpty(t, p.ID)
 	assert.Equal(t, "Product 1", p.Name)
-	assert.Equal(t, 10, p.Price)
+	assert.Equal(t, 10.0, p.Price)
 }
 
 func TestProductWhenNameIsRequired(t *testing.T) {
-	p, err := NewProduct("", 10)
+	p, err := NewProduct("", 10.0)
 	assert.Nil(t, p)
 	assert.Equal(t, ErrNameIsRequired, err)
 }
@@ -28,13 +28,13 @@ func TestProductWhenPriceIsRequired(t *testing.T) {
 }
 
 func TestProductWhenPriceIsInvalid(t *testing.T) {
-	p, err := NewProduct("Product 1", -10)
+	p, err := NewProduct("Product 1", -10.0)
 	assert.Nil(t, p)
 	assert.Equal(t, ErrPriceIsInvalid, err)
 }
 
 func TestProductValidate(t *testing.T) {
-	p, err := NewProduct("Product 1", 10)
+	p, err := NewProduct("Product 1", 10.0)
 	assert.Nil(t, err)
 	assert.NotEmpty(t, p.ID)
 	assert.Nil(t, p.Validate())
